f9b66528f2a267487186fd5706c94dc75f43e4fb
Zander2895 <jacquesalexander71@gmail.com>
2024-07-01 22:31:55 -0400

    new commit

--- shopping_list.py
+++ shopping_list.py
@@ -1,6 +1,31 @@
 shopping_list = []
 
-items = input("Enter an item to add to the list: ")
-shopping_list.append(items)
+def add_item(item):
+    shopping_list.append(item)
 
-print(shopping_list)
+def remove_item(item):
+    shopping_list.remove(item)
+
+def print_shopping_list():
+    for item in shopping_list:
+        print(f"Item: {item}")
+
+while True:
+    print("""
+    1. Add item
+    2. Remove item
+    3. Print shopping list
+    """)
+    user_option = int(input("Input the option that you would like to perform: "))
+    if user_option == 1:
+        user_item = input("What is the item you would like to add: ")
+        add_item(item= user_item)
+        print("The item was added to the list! ")
+    elif user_option == 2:
+        item_to_remove = input("What is the item that you would like to remove: ")
+        remove_item(item = item_to_remove)
+        print("The item was removed from the list! ")
+    elif user_option == 3:
+        print_shopping_list()
+    else:
+        print("Invalid option choosen! ")
